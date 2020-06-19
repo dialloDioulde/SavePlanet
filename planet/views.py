aa7c3f679908727d83c985a9fdfbc4f8d446d9e5
DIALLO Diouldé <diouldmariia@hotmail.fr>
2020-06-19 02:31:32 +0200

    Lien vers des Posts de même Catégories

--- planet/views.py
+++ planet/views.py
@@ -4,7 +4,7 @@ from planet import model_helpers, navigation
 
 # Create your views here.
 
-
+# Renvoie tous les Posts et on affiche les plus récents
 def post_list(request, category_name = model_helpers.post_category_all.slug()):
 
     category, posts = model_helpers.get_category_and_posts(category_name)
@@ -16,8 +16,13 @@ def post_list(request, category_name = model_helpers.post_category_all.slug()):
     return render(request, 'planet/post_list.html', context)
 
 
+# Renvoie les details d'un Post
 def post_detail(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
+    post = get_object_or_404(Post, pk=post_id) # On récupère le Post par son ID
 
-    context = {'post' : post, 'navigation_items' : navigation.navigation_items(navigation.NAV_POSTS)}
+    # On récupère que les Posts d'une même Catégorie ici
+    posts_same_category = Post.objects.filter(published = True, category = post.category).exclude(pk = post_id)
+
+    context = {'post' : post, 'navigation_items' : navigation.navigation_items(navigation.NAV_POSTS),
+               'posts_same_category' : posts_same_category}
     return render(request, 'planet/post_detail.html', context)
